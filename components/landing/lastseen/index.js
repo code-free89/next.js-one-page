1a1ec865bf08b5597293b363470517425e756362
Prince <prince891028@gmail.com>
2021-06-09 17:29:22 +0300

    bg color and image alt update

--- components/landing/lastseen/index.js
+++ components/landing/lastseen/index.js
@@ -3,13 +3,13 @@ import { lastseen } from '../../../mockdata/lastseensection'
 
 export default function LastseenSection() {
   return (
-    <div className="w-full h-36 bg-gray-50 px-16 py-4 container flex flex-wrap items-stretch">
+    <div className="w-full h-36 px-16 py-4 container flex flex-wrap items-stretch">
       <div className="w-full text-center text-lg">Last seen in ...</div>
       <div className={`w-full items-center grid grid-cols-${lastseen.length}`}>
         {
           lastseen.map((item, index) => (
             <div key={`lastseen-${index}`} className="w-full flex justify-center">
-              <img src={item} className="h-8" />
+              <img alt="" src={item} className="h-8" />
             </div>
           ))
         }
