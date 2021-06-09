43f1556d76d1fcb2781a8b8452b3a7a7427f5631
Prince <prince891028@gmail.com>
2021-06-09 17:29:37 +0300

    image alt

--- components/landing/information/ProductInformation.js
+++ components/landing/information/ProductInformation.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function ProductInformation(props) {
   return (
     <div className="flex items-center space-x-3 mx-auto">
-      <img src={props.imagePath} className="w-20 h-20" />
+      <img alt="" src={props.imagePath} className="w-20 h-20" />
       <div>
       <div className="text-sm font-semibold">{props.title}</div>
       <div className="text-sm">{props.description}</div>
