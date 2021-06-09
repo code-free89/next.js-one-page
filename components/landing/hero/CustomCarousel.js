12e954765ced57517bc9dd9d3b810258f6e8426d
Prince <prince891028@gmail.com>
2021-06-09 10:45:25 +0300

    carousel height update

--- components/landing/hero/CustomCarousel.js
+++ components/landing/hero/CustomCarousel.js
@@ -5,7 +5,7 @@ import { product_details } from '../../../mockdata/herosection'
 export default function CustomCarousel() {
   return (
     <div className="px-8 mt-20">
-      <ImageGallery items={product_details.mockupImages} showFullscreenButton={false} useBrowserFullscreen={false} showPlayButton={false} lazyLoad={true} />
+      <ImageGallery items={product_details.mockupImages} showFullscreenButton={false} useBrowserFullscreen={false} showPlayButton={false} />
     </div>
   )
 }
